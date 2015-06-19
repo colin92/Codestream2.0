2691d65ddac5daa1fae05f3bb3ffbd7e4b83e869
Colin Meret <colinmeret@gmail.com>
2015-06-19 12:50:13 -0400

    updates to dbname in server/db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 var Promise = require('bluebird');
 var chalk = require('chalk');
-var dbName = "meaniscule-app";
+var dbName = "codestream2";
 
 var DATABASE_URI = "mongodb://localhost:27017/" + dbName;
 
