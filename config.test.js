d75d82d7f7798bbaea60fa3696e8734dc1578e4a
Colin Meret <colinmeret@gmail.com>
2015-06-23 16:37:39 -0400

    fixed environment variable name in config.test

--- config.test.js
+++ config.test.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbName: process.env.dbName
+  dbName: process.env.DB_URI,
   google: {
     secret_key: process.env.GOOGLE_CLIENT_SECRET,
     token: process.env.GOOGLE_CLIENT_ID,
