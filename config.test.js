763d166585f0c2b0b65210178fc6b5c24e3424ed
Colin Meret <colinmeret@gmail.com>
2015-06-23 16:27:48 -0400

    added config.test.js for test environment

---
+++ config.test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dbName: process.env.dbName
+  google: {
+    secret_key: process.env.GOOGLE_CLIENT_SECRET,
+    token: process.env.GOOGLE_CLIENT_ID,
+    callbackURL: 'http://localhost:4545/auth/google/return',
+    realm: 'http://localhost:4545/'
+  },
+  test: {
+    dbURI: 'mongodb://localhost:27017/codestream2-test'
+  }
+};
+
+
