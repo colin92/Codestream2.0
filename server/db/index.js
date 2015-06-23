471da753fda346191d99e9bc8693f333ec237a9b
Colin Meret <colinmeret@gmail.com>
2015-06-23 16:09:48 -0400

    backend of oauth now functional

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 var Promise = require('bluebird');
 var chalk = require('chalk');
-var dbName = "codestream2";
+var dbName = require('../../config.js').dbName;
 
 var DATABASE_URI = "mongodb://localhost:27017/" + dbName;
 
