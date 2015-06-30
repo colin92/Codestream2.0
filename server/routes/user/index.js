def0112e75d7941537ad50d8fc390b81a254811c
Ash Ryan <ash@ashryanbeats.com>
2015-06-30 18:57:51 -0400

    Logic handled by middleware.

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -17,90 +17,21 @@ router.use('/', function(req, res, next) {
   }
 });
 
-// GET /api/user/login    
-router.get('/login', function(req, res, next) {
-  res.redirect('/auth/google');
-});
-
 // GET /api/user/logout
 router.get('/logout', function(req, res, next) {
   res.logout();
   res.redirect('/');
 });
 
-// GET /api/user/:id
-router.get('/:id', function(req, res, next) {
-  if (req.user.id === req.params.id) {
-    User.findOne({ _id: req.params.id }).exec()
-    .then(function(user) {
-      res.status(200).send(user);
-    })
-    .then(null, next);
-  }
-  else next();
-});
 
-// PUT /api/user/:id
-router.put('/:id', function(req, res, next) {
-  if (req.user.id === req.params.id) {  
-    User.findById(req.params.id).exec()
-    .then(function(user) {
-      Object.keys(req.body).forEach(function(prop) {
-        if(prop !== '_id') {
-          user[prop] = req.body[prop];
-        }
-      });
-      user.save(function(err) {
-        if(err) return res.send(err);
-        res.status(201).send(user);
-      });
-    })
-    .then(null, next);
-  }
-  else next();
-});
-
-// DELETE /api/user/:id
-router.delete('/:id', function(req, res, next) {
-  if (req.user.id === req.params.id) {
-    User.remove({ _id: req.params.id })
-    .then(function(result) {
-      req.logout();
-      res.status(201).send(result);
-    })
-    .then(null, next);
-  }
-  else next();
-});
-
-
-// ADMINs only
-router.use('/', function(req, res, next) {
-  if(req.user.isAdmin()) {
+// ADMINs or self only
+router.use('/:id', function(req, res, next) {
+  if(req.user.isAdmin() || req.user.id === req.params.id) {
     next();
   }
   else {
     res.sendStatus(401);
   }
-});
-
-// GET /api/user/
-router.get('/', function(req, res, next) {
-  User.find({}).exec()
-  .then(function(users) {
-    return res.status(200).send(users);
-  })
-  .then(null, next);
-});
-
-// POST /api/user/    
-router.post('/', function(req, res, next) {
-  // This route should not need to be used, users are created through Oauth login
-  User.create(req.body)
-  .then(function(user) {
-    return res.status(201).send(user);
-  })
-  .then(null, next);
 });
 
 router.get('/:id', function(req, res, next) {
@@ -135,4 +66,34 @@ router.delete('/:id', function(req, res, next) {
       return res.status(201).send(result);
     })
     .then(null, next);
+});
+
+
+// ADMINs only
+router.use('/', function(req, res, next) {
+  if(req.user.isAdmin()) {
+    next();
+  }
+  else {
+    res.sendStatus(401);
+  }
+});
+
+// GET /api/user/
+router.get('/', function(req, res, next) {
+  User.find({}).exec()
+  .then(function(users) {
+    return res.status(200).send(users);
+  })
+  .then(null, next);
+});
+
+// POST /api/user/    
+router.post('/', function(req, res, next) {
+  // This route should not need to be used, users are created through Oauth login
+  User.create(req.body)
+  .then(function(user) {
+    return res.status(201).send(user);
+  })
+  .then(null, next);
 });
